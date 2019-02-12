07531c4c5015080c609008da45ecbbd5bbde978b
Xavier Re4 <xrthefrench@gmail.com>
2019-02-12 14:42:43 +0100

    3.3. Découverte de Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/secret'
   devise_for :users
   get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
